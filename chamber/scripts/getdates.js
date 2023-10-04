559565482073084ecfd58c30edb53cbed924b7d0
Justin Lee <justinmlee87@gmail.com>
2023-10-04 17:08:58 -0400

    edits

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -2,3 +2,11 @@
 
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('lastModified').textContent = new Date().toLocaleDateString('en-US', options);
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
