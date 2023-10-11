ac9afd4427ca43bebaddd079780f390e355d20e5
Justin Lee <justinmlee87@gmail.com>
2023-10-11 12:49:05 -0400

    light and dark view

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -10,3 +10,40 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+const nav = document.querySelector("nav");
+const footer = document.querySelector("footer");
+const lastModified = document.querySelector("#lastModified");
+const p = document.querySelector("p");
+const paragraph = document.querySelector("#paragraph li");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🌑")) {
+		main.style.background = "#000";
+		main.style.color = "#000";
+		nav.style.background = "purple";
+		nav.style.color = "gold";
+		footer.style.background = "purple";
+		footer.style.color = "gold";
+		lastModified.style.color = "white";
+		p.style.color = "white";
+		paragraph.style.color = "white";
+		navigation.style.background = "black";
+		modeButton.textContent = "☀️";
+	} else {
+		main.style.background = "#fff";
+		main.style.color = "#fff";
+		nav.style.background = "gold";
+		nav.style.color = "purple";
+		footer.style.background = "gold";
+		footer.style.color = "purple";
+		lastModified.style.color = "black";
+		p.style.color = "black";
+		paragraph.style.color = "black";
+		navigation.style.background = "purple";
+		modeButton.textContent = "🌑";
+	}
+});
