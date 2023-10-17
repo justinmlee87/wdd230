9995bc91a6988fcaf5c3143fc04bb33b3c438c0c
Justin Lee <justinmlee87@gmail.com>
2023-10-17 16:47:51 -0400

    chamber

---
+++ chamber/scripts/windchill.js
@@ -0,0 +1,7 @@
+var temp = 71;
+var wSpeed = 11;
+var windChill = (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+
+
+var windChill= Math.round(windChill);
+document.getElementById("windChill").innerHTML= windChill;
