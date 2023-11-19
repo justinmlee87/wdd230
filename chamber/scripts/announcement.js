04a8a523d19fb31f2e6cad0abc90e6d9bc8e8cc7
Justin Lee <justinmlee87@gmail.com>
2023-11-18 23:02:50 -0500

    banner updates

--- chamber/scripts/announcement.js
+++ chamber/scripts/announcement.js
@@ -1,24 +1,11 @@
-const dismissButton = document.querySelector(".announcement-button");
-const announcement = document.querySelector(".announcement");
 
-// Key for what days to display message
-let displayDays = {
-    0: false, // Sunday
-    1: true, // Monday
-    2: true, // Tuesday
-    3: true, // Wednesday
-    4: false, // Thursday
-    5: false, // Friday
-    6: false // Saturday
+const today = new Date().getDay();
+const banner = document.getElementById('chamber-banner');
+
+if (today >= 1 && today <= 3){
+    banner.style.display = 'block';
 }
 
-let dayOfWeek = new Date() // Get today's date
-dayOfWeek = dayOfWeek.getDay() //Gets the day of the week 0 = Sunday and 6 = Saturday
-
-if (!displayDays[dayOfWeek]) { // Look up display boolean
-    announcement.classList.toggle("none");
+function closeBanner(){
+    banner.style.display = 'none';
 }
-
-dismissButton.addEventListener("click", () => {
-    announcement.classList.toggle("none");
-});
